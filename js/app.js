ee95f44259e483927a6dd5cf9382e902dcdbe10e
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-09-11 12:30:39 +0600

    modal title added done

--- js/app.js
+++ js/app.js
@@ -51,14 +51,15 @@ const addToCart = (id, price) => {
 };
 
 const showProductDetails = (product_id) => {
-   console.log(product_id);
+   
    fetch(`https://fakestoreapi.com/products/${product_id}`)
       .then((res) => res.json())
       .then((data) => showProductDetailsInModal(data));
 };
 
 const showProductDetailsInModal = (product_details) => {
-   console.log(product_details);
+   // console.log(product_details.id)
+
    setInnerText('exampleModalLabel', product_details.title);
    setInnerText('product_id', product_details.id);
    setInnerText('modal_body', product_details.description);
@@ -81,7 +82,8 @@ const updatePrice = (id, value) => {
 
 // set innerText function
 const setInnerText = (id, value) => {
-   document.getElementById(id).innerText = Math.round(value);
+ 
+   document.getElementById(id).innerText = value;
 };
 
 // update delivery charge and total Tax
